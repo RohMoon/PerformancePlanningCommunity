f09ebcba99ef8768ff5f15f08525a186dad50e68
RohMoon <rs.moony@gmail.com>
2023-12-02 23:20:33 +0900

    Initial commit

---
+++ src/main/java/moon/v1/performanceplanningcommunity/PerformancePlanningCommunityApplication.java
@@ -0,0 +1,13 @@
+package moon.v1.performanceplanningcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformancePlanningCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformancePlanningCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moon/v1/performanceplanningcommunity/PerformancePlanningCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package moon.v1.performanceplanningcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformancePlanningCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
