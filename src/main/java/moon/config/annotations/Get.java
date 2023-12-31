da2a8007349aa2c5f2ccf6e0f80d456d43ce8e2d
SangMoon <moonyroh@weshareart.com>
2023-12-31 20:11:23 +0900

    Rest API 커스터 마이즈 어노테이션 추가

---
+++ src/main/java/moon/config/annotations/Get.java
@@ -0,0 +1,51 @@
+package moon.config.annotations;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ResponseStatus(HttpStatus.OK)
+@RequestMapping(method = RequestMethod.GET)
+public @interface Get {
+    @AliasFor(
+            annotation = RequestMapping.class
+    )
+    String name() default "";
+
+    @AliasFor(
+            annotation = RequestMapping.class
+    )
+    String[] value() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class
+    )
+    String[] path() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class
+    )
+    String[] params() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class
+    )
+    String[] headers() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class
+    )
+    String[] consumes() default {};
+
+    @AliasFor(
+            annotation = RequestMapping.class
+    )
+    String[] produces() default {};
+}
